cae00374a5f2a10734ba3d2dcc6e8e0432d7077a
Michael <urmich.m@gmail.com>
2022-01-31 19:03:18 +0100

    DianaTest test removed dummy request will be tested in the next release, once network.service updated

--- Diana/src/test/java/com/github/urmichm/diana/DianaTest.kt
+++ Diana/src/test/java/com/github/urmichm/diana/DianaTest.kt
@@ -11,34 +11,34 @@ import org.junit.Test
 class DianaTest {
 
 
-    @Test
-    fun nearbySearch() {
-        val PLACES_API_KEY : String = YOUR_KEY
-        var complete = false
-
-        val type   = "tourist_attraction"
-        val latLng = "52.249787,21.012575"
-        val rankby = "distance"
-
-        val diana = Diana.Builder(PLACES_API_KEY)
-            .setVicinity2Address(true)
-            .build()
-
-        CoroutineScope(Dispatchers.IO).launch{
-            val response = diana.nearbySearch(type, latLng, rankby)
-            response?.let {
-                assertEquals("OK",  response.status)
-                val p = response.results[0].asPlace()
-
-//                println("All results:\n${response.results}")
-//                println(p)
-
-                assertEquals(response.results[0].name, p.name)
-                complete = true
-            }
-        }
-
-        Thread.sleep(5_000)
-        assertTrue(complete)
-    }
+//    @Test
+//    fun nearbySearch() {
+//        val PLACES_API_KEY : String = YOUR_KEY
+//        var complete = false
+//
+//        val type   = "tourist_attraction"
+//        val latLng = "52.249787,21.012575"
+//        val rankby = "distance"
+//
+//        val diana = Diana.Builder(PLACES_API_KEY)
+//            .setVicinity2Address(true)
+//            .build()
+//
+//        CoroutineScope(Dispatchers.IO).launch{
+//            val response = diana.nearbySearch(type, latLng, rankby)
+//            response?.let {
+//                assertEquals("OK",  response.status)
+//                val p = response.results[0].asPlace()
+//
+////                println("All results:\n${response.results}")
+////                println(p)
+//
+//                assertEquals(response.results[0].name, p.name)
+//                complete = true
+//            }
+//        }
+//
+//        Thread.sleep(5_000)
+//        assertTrue(complete)
+//    }
 }
