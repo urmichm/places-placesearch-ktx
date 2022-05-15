481c81306722184dc1ca1e9196ed86690eb3df1e
Michael <urmich.m@gmail.com>
2022-05-15 10:18:06 +0200

    (III) Extensions.kt updated Place.Field.toRequestString completed

--- Diana/src/main/java/com/github/urmichm/diana/Extensions.kt
+++ Diana/src/main/java/com/github/urmichm/diana/Extensions.kt
@@ -14,12 +14,20 @@ internal fun LatLng.toRequestString():String{
 
 /**
  * Convert [Place.Field] object to String for API requests
- * @return String object of LatLng fot API requests
- * TODO: Complete the method
+ * @return String object of [Place.Field] fot API requests
  * */
 internal fun Place.Field.toRequestString():String{
     return when(this){
-
+        Place.Field.ADDRESS_COMPONENTS -> "address_component"
+        Place.Field.ADDRESS -> "formatted_address"
+        Place.Field.VIEWPORT -> "geometry/viewport"
+        Place.Field.LAT_LNG -> "geometry/location"
+        Place.Field.ICON_URL -> "icon_mask_base_uri"
+        Place.Field.PHOTO_METADATAS -> "photos"
+        Place.Field.ID -> "place_id"
+        Place.Field.TYPES -> "type"
+        Place.Field.PHONE_NUMBER -> "international_phone_number"
+        Place.Field.WEBSITE_URI -> "website"
         else -> this.toString().lowercase()
     }
 }
