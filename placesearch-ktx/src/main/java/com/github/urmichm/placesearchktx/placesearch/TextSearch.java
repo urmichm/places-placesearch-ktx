c8bb8b28cfef900cbae44621c694b72260bd2f92
Michael <urmich.m@gmail.com>
2022-05-30 13:32:35 +0200

    TextSearch module is Kotlin now!

--- placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/placesearch/TextSearch.java
+++
@@ -1,9 +0,0 @@
-package com.github.urmichm.placesearchktx.placesearch;
-
-
-/**
- * @brief Text Search Google API
- * @details https://developers.google.com/maps/documentation/places/web-service/search-find-place
- * */
-public class TextSearch {
-}
---
+++ placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/placesearch/TextSearch.kt
@@ -0,0 +1,8 @@
+package com.github.urmichm.placesearchktx.placesearch
+
+/**
+ * Text Search Google API
+ * @details https://developers.google.com/maps/documentation/places/web-service/search-find-place
+ * */
+class TextSearch {
+}
