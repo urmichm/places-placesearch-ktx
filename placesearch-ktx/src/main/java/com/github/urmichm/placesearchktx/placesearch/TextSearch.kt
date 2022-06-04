e2bfe87da7fff48aa329ccb992146e0113e1111b
Michael <urmich.m@gmail.com>
2022-06-04 21:02:05 +0200

    cosmetic changes

--- placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/placesearch/TextSearch.kt
+++ placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/placesearch/TextSearch.kt
@@ -1,14 +1,31 @@
 package com.github.urmichm.placesearchktx.placesearch
 
-import com.github.urmichm.placesearchktx.containers.NearbySearchContainer
+import com.github.urmichm.placesearchktx.Diana
 import com.github.urmichm.placesearchktx.containers.TextSearchContainer
 
 /**
  * Text Search Google API
  * @details https://developers.google.com/maps/documentation/places/web-service/search-text
+ * @param builder The [Builder] object
  * */
-class TextSearch(){
+class TextSearch private constructor(private val builder: Builder){
+    /**
+     * [Diana] object with general settings
+     * */
+    private val diana : Diana = builder.diana
 
+
+    class Builder(val diana : Diana){
+
+
+        /**
+         * The build method to create a [TextSearch] object
+         * @return [TextSearch] object created according to the builder settings.
+         * */
+        fun build() :TextSearch{
+            return TextSearch(this)
+        }
+    }
 
     /**
      * Make a call to Text Search.
