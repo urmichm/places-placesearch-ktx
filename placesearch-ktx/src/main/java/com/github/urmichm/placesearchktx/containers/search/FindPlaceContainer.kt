838d278e95e9a7c28c625c5c9e83bc63f5b7da33
Michael <urmich.m@gmail.com>
2022-06-09 20:27:39 +0200

    comments moved

--- placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/containers/search/FindPlaceContainer.kt
+++ placesearch-ktx/src/main/java/com/github/urmichm/placesearchktx/containers/search/FindPlaceContainer.kt
@@ -12,33 +12,12 @@ import com.squareup.moshi.Json
  * @details https://developers.google.com/maps/documentation/places/web-service/search-find-place
  * */
 class FindPlaceContainer (
-
-    /**
-     * Contains an array of Place candidates.
-     */
     @Json(name="candidates")
     override val places :List<PlaceDetailsContainer>,
-
-    /**
-     * Contains the status of the request, and may contain debugging information to help you track down why the request failed.
-     */
     @Json(name="status")
     override val status : String,
-
-    /**
-     * When the service returns a status code other than OK<, there may be an additional error_message field within the response object.
-     * This field contains more detailed information about thereasons behind the given status code.
-     * This field is not always returned, and its content is subject to change.
-     */
     @Json(name="error_message")
     override val errorMessage : String?,
-
-    /**
-     * When the service returns additional information about the request specification,
-     * there may be an additional info_messages field within the response object.
-     * This field is only returned for successful requests. It may not always be returned,
-     * and its content is subject to change.
-     */
     @Json(name="info_messages")
     override val infoMessages : List<String>?,
 
