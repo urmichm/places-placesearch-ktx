6bafd8165855075d05a6e99368172b642d4ab9ce
Michael <urmich.m@gmail.com>
2022-06-26 16:52:07 +0200

    Menu added Find Place example added

--- app/src/main/java/com/urmich/android/demo/MapsActivity.kt
+++ app/src/main/java/com/urmich/android/demo/MapsActivity.kt
@@ -2,6 +2,11 @@ package com.urmich.android.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +15,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.urmich.android.demo.databinding.ActivityMapsBinding
+import com.urmich.android.placesearchktx.placesearch.search.FindPlace
+import com.urmich.android.placesearchktx.placesearch.search.NearbySearch
+import com.urmich.android.placesearchktx.placesearch.search.TextSearch
+import kotlinx.coroutines.launch
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val TAG = "MapsActivity"
+    private val homeLatLng = LatLng(52.247551,21.0126315)
+    private val zoomLevel = 15f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +53,68 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+
+        mMap.setOnMapClickListener {
+            mMap.clear()
+        }
     }
+
+    /**
+     * Example of Find Place usage
+     * */
+    private fun runFindPlace(){
+
+        val findPlace = FindPlace.Builder()
+            .setInput("museums")
+            .setInputType(FindPlace.InputType.TEXTQUERY)
+            .setLocationBias(homeLatLng)
+            .setFields(listOf(FindPlace.Field.NAME, FindPlace.Field.LOCATION))
+            .setLanguage("en")
+            .build()
+        
+        lifecycleScope.launch { 
+            val response = findPlace.call()
+            Log.i(TAG, response.toString())
+            if(response?.status == "OK"){
+                response.places.forEach { it ->
+                    mMap.addMarker(
+                        MarkerOptions()
+                            .position(it.asPlace().latLng!!)
+                            .title(it.asPlace().name)
+                    )
+                }
+            }
+        }
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        mMap.clear()
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.find_place -> {
+                runFindPlace()
+                Log.i(TAG, FindPlace.TAG)
+                true
+            }
+            R.id.nearby_search -> {
+                Log.i(TAG, NearbySearch.TAG)
+                true
+            }
+            R.id.text_search -> {
+                Log.i(TAG, TextSearch.TAG)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
